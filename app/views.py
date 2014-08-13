e2585b1a5ec87b2d327482ac4203af6f5c4abe92
Genevieve Kathleen Smith <genevieve.smith@utexas.edu>
2014-08-13 19:14:56 -0400

    Working through sql insert quirks, adding barchart to front end

--- app/views.py
+++ app/views.py
@@ -1,27 +1,46 @@
-from flask import render_template
+from flask import render_template, Flask, jsonify
 from app import app
 import pymysql as mdb
 
-db = mdb.connect(user='root', host='localhost', db='world_innodb', charset='utf8')
+db = mdb.connect(user="root", host="localhost", db="world_innodb", charset='utf8')
+
+fcc_db = mdb.connect(user="root", host="localhost", db="FCC", charset="utf8")
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template("index.html",
-    	title = 'Home',
-    	user = { 'nickname': 'Miguel' },
-    	)
+	return render_template("index.html",
+        title = 'Home')
 
-@app.route('/db')
-def cities_page():
+@app.route("/jquery")
+def index_jquery():
+    return render_template('index_js.html') 
+
+@app.route("/neutral")
+def index_neutral():
+    return render_template('greyscale/index.html') 
+
+@app.route("/db_json")
+def cities_json():
 	with db:
 		cur = db.cursor()
-		cur.execute('SELECT Name FROM city LIMIT 15;')
+		cur.execute("SELECT Name, CountryCode, Population FROM city ORDER BY Population DESC;")
 		query_results = cur.fetchall()
-	cities = ''
+	cities = []
+	for result in query_results:
+		cities.append(dict(name=result[0], country=result[1], population=result[2]))
+	return jsonify(dict(cities=cities))
+    
+@app.route('/db')
+def cities_page():
+	with db: 
+		cur = db.cursor()
+		cur.execute("SELECT Name FROM city LIMIT 15;")
+		query_results = cur.fetchall()
+	cities = ""
 	for result in query_results:
 		cities += result[0]
-		cities += '<br>'
+		cities += "<br>"
 	return cities
 
 @app.route("/db_fancy")
@@ -35,3 +54,23 @@ def cities_page_fancy():
 	for result in query_results:
 		cities.append(dict(name=result[0], country=result[1], population=result[2]))
 	return render_template('cities.html', cities=cities) 
+
+
+@app.route("/neutralopinion")
+def index_neutralopinion():
+    return render_template('index_grayscale.html') 
+
+
+
+@app.route("/states_fancy")
+def states_page_fancy():
+	with fcc_db:
+		cur = fcc_db.cursor()
+		cur.execute("SELECT state, comments from FCC;")
+
+		query_results = cur.fetchall()
+	states = []
+	for result in query_results:
+		states.append(dict(state=result[0], comments=result[1]))
+	return render_template('states.html', states=states) 
+
---
+++ timeline.R
@@ -0,0 +1,42 @@
+
+
+data = read.csv('Documents/insight/raw_metadata.csv')
+
+state_totals = table(data$state)
+
+head(data)
+plot(data$date[1:5])
+
+summary(data)
+class(data$date[1])
+
+as.date(data$date)
+
+?date
+strptime(data$date[1], "%Y-%m-%d %H:%M:%S"))
+strptime(data$date[1], "%Y-%m-%d %H:%M:%S")
+
+true_date = as.POSIXlt(strptime(data$date, "%Y-%m-%dT%H:%M:%SZ"))
+plot(true_date,rep(1,dim(data)[1]))
+dim(data)
+
+
+days = round(true_date,"day")
+days[1]
+table(days)
+length(days)
+class(days)
+
+days2 = as.character(days)
+days3 = table(days2)
+max(days3)
+length(days3)
+days_of_interest = as.POSIXlt(strptime(labels(days3)$days2, "%Y-%m-%d"))
+plot(days_of_interest,days3,type='p',ylim=c(0,max(days3)),cex=.5)
+
+plot(days_of_interest,days3)
+
+p = plot(days3,ylab='Number of comments')
+abline()
+names(p)
+p
