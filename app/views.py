1a77804085dea021ff37bbb340c6373a48957f33
Genevieve Kathleen Smith <genevieve.smith@utexas.edu>
2014-08-25 10:03:20 -0400

    Fixing db placement in views file

--- app/views.py
+++ app/views.py
@@ -4,7 +4,6 @@ from flask import render_template, Flask, jsonify, request
 import pymysql as mdb
 app = Flask(__name__)
 
-fcc_db = mdb.connect(user="root", host="localhost", db="FCC", charset="utf8")
 
 @app.route('/')
 @app.route('/index')
@@ -13,6 +12,7 @@ def index():
 
 @app.route("/fcc_db_json/")
 def whatever():
+	fcc_db = mdb.connect(user="root", host="localhost", db="FCC", charset="utf8")
 	with fcc_db:
 		cur = fcc_db.cursor()
 		cur.execute("SELECT state, comment_rate, sentiment_score, duplication_rate FROM states;")
@@ -25,6 +25,7 @@ def whatever():
 
 @app.route("/fcc_db_json_test/", methods=["GET"])
 def anything():
+	fcc_db = mdb.connect(user="root", host="localhost", db="FCC", charset="utf8")
 	print 'hello'
 	inputstate = request.args.get('inputstate')
 	with fcc_db:
@@ -32,9 +33,9 @@ def anything():
 		cur.execute("SELECT state, comment_rate, sentiment_score, duplication_rate, rank, average, sent_average, full_name FROM states WHERE state = '%s'" %(inputstate))
 		query_results = cur.fetchall()
 		mydict = {'state':query_results[0][0], 
-			'comment_rate':query_results[0][1], 
-			'sentiment_score':query_results[0][2], 
-			'duplication_rate':query_results[0][3],
+			'comment_rate':round(query_results[0][1],2), 
+			'sentiment_score':round(query_results[0][2],2), 
+			'duplication_rate':round(query_results[0][3]*100,1),
 			'comment_rank':query_results[0][4],
 			'comment_average':query_results[0][5],
 			'sent_avergae':query_results[0][6],
@@ -44,5 +45,5 @@ def anything():
 
 
 if __name__ == "__main__":
-	app.run(debug = True)
+	app.run(host='0.0.0.0',port=5000)
 
